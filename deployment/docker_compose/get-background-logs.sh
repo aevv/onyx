3a3696c366c1c31fbdcc36f64be3b40fc49c2766
MattThompson <matt@aevv.net>
2025-02-01 19:35:03 +0000

    proper bash variable assignment

--- deployment/docker_compose/get-background-logs.sh
+++ deployment/docker_compose/get-background-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-container = $(sudo docker container ls | grep background | awk '{print $1}')
+container=$(sudo docker container ls | grep background | awk '{print $1}')
 
 sudo docker container exec -it $container cat /var/log/onyx_info.log | grep 'codat:'
